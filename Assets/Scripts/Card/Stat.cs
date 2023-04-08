24f032fe28a49808472c97cb268d8252ff37e21c
ApotoxinElish <19079181d@connect.polyu.hk>
2023-04-08 20:43:33 +0800

    Update

---
+++ Assets/Scripts/Card/Stat.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace AHLCG
+{
+    /// <summary>
+    /// The modifier of a stat.
+    /// </summary>
+    public class Stat : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
