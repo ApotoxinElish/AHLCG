704398709c20e61ed2e8b10a102cafb67596a6f4
ApotoxinElish <19079181d@connect.polyu.hk>
2023-04-14 18:56:43 +0800

    Update Stat.cs

--- Assets/Scripts/Card/Stat.cs
+++ Assets/Scripts/Card/Stat.cs
@@ -8,18 +8,46 @@ namespace AHLCG
     /// <summary>
     /// The modifier of a stat.
     /// </summary>
-    public class Stat : MonoBehaviour
+    public class Modifier
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        /// <summary>
+        /// The constant value to identify a permanent modifier.
+        /// </summary>
+        private const int PERMANENT = 0;
 
+        /// <summary>
+        /// The value of this modifier.
+        /// </summary>
+        public int value;
+
+        /// <summary>
+        /// The duration of this modifier.
+        /// </summary>
+        public int duration;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="value">The value of the modifier.</param>
+        /// <param name="duration">The duration of the modifier.</param>
+        public Modifier(int value, int duration = PERMANENT)
+        {
+            this.value = value;
+            this.duration = duration;
         }
 
-        // Update is called once per frame
-        void Update()
+        /// <summary>
+        /// Returns true if this modifier is permanent and false otherwise.
+        /// </summary>
+        /// <returns>True if this modifier is permanent; false otherwise.</returns>
+        public bool IsPermanent()
         {
-
+            return duration == PERMANENT;
         }
+    }
+
+    public class Stat
+    {
+
     }
 }
